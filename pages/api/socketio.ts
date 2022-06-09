cb315503d1470111cd96def638efe98242c2c16e
Jihwan Kim <jihwan.kim@purple.io>
2022-06-10 02:19:57 +0900

    -

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -1,23 +1,48 @@
-import { Server } from "socket.io";
-let isConnected = false
-const ioHandler = (_req: any, res: any) => {
-  if(!isConnected){
-    isConnected = true
+import { Server, Socket } from "socket.io";
+import moment from "moment";
+let isConnected = false;
+
+const ioHandler = (req: any, res: any) => {
+  if (!isConnected) {
+    isConnected = true;
     const io = new Server(res.socket.server);
     console.log("*First use, starting socket.io");
     io.on("connection", (socket) => {
-      console.log('소켓시작')
-      socket.broadcast.emit("a user connected");
+      console.log(`socket-id : ${socket.id}`);
+      console.log(`client-id : ${socket.client.id}`);
+      let tempColor = "";
+      for (let i = 0; i < 3; i++) {
+        if (i === 2) {
+          tempColor += Math.floor(Math.random() * 256).toString();
+        } else {
+          tempColor += Math.floor(Math.random() * 256).toString() + ",";
+        }
+      }
+      const tempId = socket.client.id;
+      const personalInfo = { id: tempId, color: tempColor };
+      io.emit("profile", personalInfo);
+      socket.on("disconnect", () => {
+        console.log("user has left");
+      });
+      console.log("소켓시작");
+      // console.log(`현재인원 : ${socket.conn.server.clientsCount}`);
+      console.log("---");
       socket.on("message", (body) => {
-        const {name,message} = body
+        const { name, message } = body;
+        const msg = {
+          name,
+          message,
+          color: tempColor,
+          time: moment().format("LT"),
+        };
         console.log(`[${name}] - got message : ${message}`);
-        console.log(`현재인원 : ${socket.conn.server.clientsCount}`);
-        console.log('---');
-        
-        io.emit("message", body)
+        // console.log(`현재인원 : ${socket.conn.server.clientsCount}`);
+        console.log("---");
+
+        io.emit("message", msg);
       });
     });
   }
-  res.status(200).json({message:'socket connected'})
+  res.status(200).json({ message: "socket connect success" });
 };
 export default ioHandler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,180 @@
-import { connect } from "http2";
+import { Button, Input, Form } from "antd";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useState, createRef } from "react";
 import io from "socket.io-client";
-
-// const socket = io.connect("http://localhost:4000");
 interface IHistory {
   name: string;
   message: string;
+  time: string;
+  color: string;
 }
+
 const socket = io();
 const Home: NextPage = () => {
+  const [form] = Form.useForm();
   const [message, setMessage] = useState("");
-  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
   const [history, setHistory] = useState<IHistory[]>([]);
-
+  const [profileColor, setProfileColor] = useState("");
+  const [id, setId] = useState("");
+  const scrollRef = createRef<HTMLDivElement>();
+  const inputRef = createRef<HTMLInputElement>();
   useEffect(() => {
     console.log("useeffect");
+    // setUserName(Math.floor(Math.random() * 1000).toString());
     const connectToSocket = async () => {
-      await fetch("/api/socketio");
-    };
+      const res = await fetch("/api/socketio");
+      const res2json = await res.json();
+      // setProfileColor(res2json.color);
+      // setId(res2json.userId);
+      // setUserName(res2json.userId.slice(0, 4));
+    };;
     connectToSocket();
+    return () => {
+      socket.emit("disconnection");
+    };
   }, []);
+  socket.on("profile", (data) => {
+    // console.log(data);
+    console.log(userName);
 
+    if (!userName) {
+      setProfileColor(data.color);
+      setUserName(data.id);
+    }
+  });
   socket.on("message", (data) => {
     const tempHistory = [...history, data];
-    setHistory(tempHistory);
+    setHistory([...tempHistory]);
   });
-
+  useEffect(() => {
+    scrollRef &&
+      scrollRef.current &&
+      scrollRef.current.scrollIntoView({
+        behavior: "smooth",
+      });
+  }, [history, scrollRef]);
   return (
     <div>
-      {history.map((data, idx) => {
-        const { name, message } = data;
-        const key = name + idx;
-        return (
-          <div key={key}>
-            <span
-              style={{ color: name === "맥북" ? "orange" : "blue" }}
-            >{`${name} : `}</span>
-            <span style={{ color: name === "맥북" ? "orange" : "blue" }}>
-              {message}
-            </span>
-          </div>
-        );
-      })}
-      <div>
-        {`이름 : `}
-        <input
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        ></input>
+      <Button
+        onClick={() => {
+          console.log(userName);
+        }}
+      >
+        dbg
+      </Button>
+      <div
+        style={{
+          height: "500px",
+          overflow: "scroll",
+          padding: "20px",
+          background: "#ededed",
+        }}
+      >
+        {history.map((data, idx) => {
+          const { name, message, time } = data;
+          const splitTime = time.split(" ");
+          const key = data + idx.toString();
+          return (
+            <div
+              key={key}
+              style={{
+                marginBottom: "10px",
+                display: "flex",
+                alignContent: "center",
+                justifyContent: name === userName ? "flex-end" : "flex-start",
+              }}
+              ref={scrollRef}
+            >
+              <div
+                style={{
+                  background: `rgb(${data.color})`,
+                  // background: `rgb(41, 191, 46)`,
+                  borderRadius: "50%",
+                  width: "30px",
+                  height: "30px",
+                  marginRight: "5px",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {name.slice(-1)}
+              </div>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ fontSize: "11px", marginBottom: "4px" }}>
+                  {name}
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "flex-end",
+                  }}
+                >
+                  <div
+                    style={{
+                      padding: "4px 10px",
+                      backgroundColor: name === userName ? "#FEE500" : "white",
+                      color: "black",
+                      borderRadius: "10px",
+                      fontSize: "14px",
+                      marginRight: "4px",
+                    }}
+                  >
+                    {message}
+                  </div>
+                  <div style={{ fontSize: "10px", color: "adadad" }}>{`${
+                    splitTime[1] === "AM" ? "오전" : "오후"
+                  } ${splitTime[0]}`}</div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div>
-        {`메시지 : `}
-        <input
-          onChange={(e) => {
-            setMessage(e.target.value);
-          }}
-        ></input>
-        <button
-          onClick={() => {
-            if (!name) {
-              alert("이름을 입력해주세요");
-              return;
-            }
-            socket.emit("message", { name, message });
-            // alert("메시지 전송 성공");
-          }}
-        >
-          전송
-        </button>
+      <div style={{ display: "flex" }}>
+        <Form form={form}>
+          <div style={{ display: "flex" }}>
+            <Form.Item
+              name="messageInput"
+              style={{ marginRight: "20px", width: "200px" }}
+            >
+              <Input
+                style={{ borderRadius: "10px" }}
+                ref={inputRef}
+                size="small"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    socket.emit("message", { name: userName, message });
+                    form.resetFields();
+                    inputRef.current.focus();
+                  }
+                }}
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                size="small"
+                type="primary"
+                shape="round"
+                onClick={() => {
+                  if (!userName) {
+                    alert("이름을 입력해주세요");
+                    return;
+                  }
+                  socket.emit("message", { name: userName, message });
+                  form.resetFields();
+                  inputRef.current.focus();
+                }}
+              >
+                전송
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
       </div>
     </div>
   );
